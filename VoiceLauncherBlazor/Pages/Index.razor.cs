bdcce34b86b92b3f89d3c7b2fed40616aa0d9695
Mark Phillipson <MPhillipson0@Gmail.com>
2021-04-02 11:12:36 +0100

    Change the alert to toast on the index

---
+++ VoiceLauncherBlazor/Pages/Index.razor.cs
@@ -0,0 +1,22 @@
+﻿using Blazored.Toast.Services;
+using DataAccessLibrary;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoiceLauncherBlazor.Pages
+{
+	public partial class Index
+	{
+		[Inject] ITodoData TodoData { get; set; }
+		[Inject] IToastService ToastService { get; set; }
+		public List<string> projects { get; set; } = new List<string>();
+		protected override async Task OnInitializedAsync()
+		{
+			projects = await TodoData.GetProjects();
+			ToastService.ShowInfo("To Blazor Server demonstration application", "Hello and Welcome");
+		}
+	}
+}
