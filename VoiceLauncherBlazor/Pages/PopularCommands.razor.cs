bc61fb4351b4b1a1a8486ea49489fb1f790df641
Mark Phillipson <MPhillipson0@Gmail.com>
2021-04-29 11:23:01 +0100

    Finished popular command links

---
+++ VoiceLauncherBlazor/Pages/PopularCommands.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Linq;
+
+namespace VoiceLauncherBlazor.Pages
+{
+	public partial class PopularCommands
+	{
+		[Inject] NavigationManager NavigationManager { get; set; }
+		void FilterCommand(string commandToFilter,string applicationToFilter="devenv")
+		{
+			commandToFilter = commandToFilter.Replace(" ", "%20");
+			NavigationManager.NavigateTo($"commandsetoverview?name={commandToFilter}&application={applicationToFilter}");
+		}
+	}
+}
