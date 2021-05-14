c4edd7873d33df5766e67e8b93f3a92da01c184f
Mark Phillipson <MPhillipson0@Gmail.com>
2021-05-14 17:54:06 +0100

    Finished YouTube links

--- VoiceLauncherBlazor/Pages/PopularCommands.razor.cs
+++ VoiceLauncherBlazor/Pages/PopularCommands.razor.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace VoiceLauncherBlazor.Pages
 {
 	public partial class PopularCommands
 	{
+		[Inject] public IJSRuntime JSRuntime { get; set; }
 		[Inject] NavigationManager NavigationManager { get; set; }
-		public bool ShowVideo { get; set; } = false;
-		void FilterCommand(string commandToFilter, string applicationToFilter = "devenv", bool showLists = false, string youtubeUrl= null )
+		public bool ShowVideo { get; set; } = true;
+		async System.Threading.Tasks.Task FilterCommandAsync(string commandToFilter, string applicationToFilter = "devenv", bool showLists = false, string youtubeUrl = null)
 		{
-			if (ShowVideo && youtubeUrl!= null )
+			if (ShowVideo && youtubeUrl != null)
 			{
-				NavigationManager.NavigateTo(youtubeUrl,true);
+				//NavigationManager.NavigateTo(youtubeUrl,true);
+				await JSRuntime.InvokeAsync<object>("open", youtubeUrl, "_blank");
 			}
 			else
 			{
