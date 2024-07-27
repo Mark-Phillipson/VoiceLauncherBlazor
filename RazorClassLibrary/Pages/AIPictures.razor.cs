057615695eca79ec2875b3190b48f67f93b0a8af
Mark Phillipson <MPhillipson0@Gmail.com>
2024-07-27 08:04:53 +0100

    feat: Add JsonRepositoryOptions class and ICursorlessCheatsheetItemJsonRepository interface

---
+++ RazorClassLibrary/Pages/AIPictures.razor.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Components;
+using OpenAI.Images;
+using VoiceLauncher;
+namespace RazorClassLibrary.Pages
+{
+    public partial class AIPictures : ComponentBase
+    {
+        string imagePrompt = "";
+        string imageUrl = "";
+        bool generatingImage = false;
+        private async Task GenerateImage()
+        {
+            if (!string.IsNullOrWhiteSpace(imagePrompt))
+            {
+                generatingImage = true;
+                var openAI = new ImageClient("dall-e-3", Constants.OpenAIAPIKEY);
+                var imageRequest = new ImageGenerationOptions
+                {
+                    Quality = GeneratedImageQuality.High,
+                    Size = GeneratedImageSize.W1024xH1024,
+                    Style = GeneratedImageStyle.Vivid,
+                    ResponseFormat = GeneratedImageFormat.Uri
+                };
+
+                var response = await openAI.GenerateImageAsync(imagePrompt, imageRequest);
+                System.Console.WriteLine(response.Value.ImageUri);
+                imageUrl = response.Value.ImageUri.ToString();
+                generatingImage = false;
+                StateHasChanged();
+            }
+        }
+    }
+}
