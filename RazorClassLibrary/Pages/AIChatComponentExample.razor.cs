26f97a10fa4ea99c400428b12e890775f8855956
Mark Phillipson <MPhillipson0@Gmail.com>
2024-08-29 09:22:00 +0100

    Further Semantic Kernel Testing

--- RazorClassLibrary/Pages/AIChatComponentExample.razor.cs
+++ RazorClassLibrary/Pages/AIChatComponentExample.razor.cs
@@ -148,19 +148,27 @@ namespace RazorClassLibrary.Pages
 public class MarkInformation
 {
     [KernelFunction]
-    public string GetInformationAboutMark(string information)
+    public string GetInformationAboutMark(string dataCategory)
     {
-        if (information == "age")
+        if (dataCategory == "age")
         {
             return "Mark Was Born on the a day in August 1964 so he is currently sixty years old";
         }
-        else if (information == "location")
+        else if (dataCategory == "location")
         {
             return "Mark's location is Maidstone Kent";
         }
-        else if (information == "job")
+        else if (dataCategory == "job")
         {
-            return "Mark's is a wannabe software developer";
+            return "Mark's is a .NET software developer freelancing on Upwork";
+        }
+        else if (dataCategory == "hobbies")
+        {
+            return "Mark's hobbies include riding the bicycle, reading and programming in C#";
+        }
+        else if (dataCategory == "surname")
+        {
+            return "Mark has a last name of Phillipson";
         }
         else
         {
