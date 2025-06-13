6502ac57dcf9002b9d0d018929fbd64b1e2c247d
Mark Phillipson <MPhillipson0@Gmail.com>
2025-06-13 10:04:05 +0100

    feat: remove Talon Voice Command Search page and integrate semantic search toggle functionality 🔍✨

--- RazorClassLibrary/Pages/TalonVoiceCommandSearch.razor.cs
+++ RazorClassLibrary/Pages/TalonVoiceCommandSearch.razor.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using DataAccessLibrary.Models;
+using SmartComponents.LocalEmbeddings;
+using System.Linq;
 
 namespace RazorClassLibrary.Pages
 {
@@ -11,6 +13,11 @@ namespace RazorClassLibrary.Pages
         public List<TalonVoiceCommand> Results { get; set; } = new();
         public bool IsLoading { get; set; }
         public bool HasSearched { get; set; }
+        public bool UseSemanticMatching { get; set; } = true;
+        private int maxResults = 20;
+
+        [Inject]
+        public DataAccessLibrary.Services.TalonVoiceCommandDataService? TalonService { get; set; }
 
         protected override void OnInitialized()
         {
@@ -22,7 +29,32 @@ namespace RazorClassLibrary.Pages
             IsLoading = true;
             HasSearched = true;
             StateHasChanged();
-            Results = await TalonService.SemanticSearchAsync(SearchTerm);
+            if (TalonService is not null)
+            {
+                var allCommands = await TalonService.SemanticSearchAsync(""); // get all
+                if (UseSemanticMatching && !string.IsNullOrWhiteSpace(SearchTerm) && SearchTerm.Length > 2)
+                {
+                    var candidates = allCommands.Select(c => (Item: c, Text: c.Command + " " + c.Script)).ToList();
+                    using var embedder = new LocalEmbedder();
+                    var matchedResults = embedder.EmbedRange(candidates.Select(x => x.Text).ToList());
+                    var results = LocalEmbedder.FindClosestWithScore(embedder.Embed(SearchTerm), matchedResults, maxResults: maxResults);
+                    var resultItems = results.Select(r => r.Item).ToList();
+                    var similarityScores = results.ToDictionary(r => r.Item, r => r.Similarity);
+                    Results = candidates
+                        .Where(c => resultItems.Contains(c.Text))
+                        .OrderByDescending(c => similarityScores[c.Text])
+                        .Select(c => c.Item)
+                        .ToList();
+                }
+                else
+                {
+                    Results = await TalonService.SemanticSearchAsync(SearchTerm);
+                }
+            }
+            else
+            {
+                Results = new List<TalonVoiceCommand>();
+            }
             IsLoading = false;
             StateHasChanged();
         }
