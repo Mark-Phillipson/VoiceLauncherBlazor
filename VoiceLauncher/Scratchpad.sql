17d600b3ea808263dc81922d88f90e74cdce53a8
Mark Phillipson <MPhillipson0@Gmail.com>
2025-10-07 10:20:56 +0100

    fix: correct SQL syntax error in ApplicationCount query 🛠️

--- VoiceLauncher/Scratchpad.sql
+++ VoiceLauncher/Scratchpad.sql
@@ -1,5 +1,5 @@
  SELECT Repository FROM TalonVoiceCommands GROUP BY Repository;
   SELECT OperatingSystem FROM TalonVoiceCommands GROUP BY OperatingSystem;
-   SELECT Application, COUNT(*) as ApplicationCount FROM TalonVoiceCommands GROUP BY Application ORDER BY 
+"TESTING""TESTING"
    ApplicationCount DESC;
     SELECT Mode FROM TalonVoiceCommands GROUP BY Mode;
